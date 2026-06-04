1e63e278410f98fb05738c36e27542e6ffe0c44e
Muhammad Jordan <linkcoln6@gmail.com>
2026-06-04 11:51:16 +0700

    Delete Python Projects directory

--- Python Projects/Band_Name_Generator.py
+++
@@ -1,4 +0,0 @@
-print("Hello and welcome to the Band Name Generator!")
-city = input("What city did you grow up in?: ")
-pet = input("What is the name of your pet?: ")
-print("Your band name is The", city + " " + pet)
--- Python Projects/Split_Bill_Calculator.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the Bill Calculator!")
-total = input("What is the total bill?: ")
-tip = input("How much do you want to tip?(10%, 12%, 15%): ")
-split = input("How many people are splitting the bill?: ")
-
-print(f"Each person should pay {(float(total) + ((float(tip)/100) * float(total))) / float(split):.2f}")
